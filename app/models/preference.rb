a4f461089833199fcff01490baacda985e734a7d
Dakota Martinez <dakotaleemusic@gmail.com>
2016-03-31 18:11:52 +0000

    up to the form specs

---
+++ app/models/preference.rb
@@ -0,0 +1,18 @@
+class Preference < ActiveRecord::Base
+    
+  def self.allow_create_artists?
+    self.first.allow_create_artists?
+  end
+  
+  def self.allow_create_songs?
+    self.first.allow_create_songs?
+  end
+  
+  def self.song_sort_order 
+    self.first.song_sort_order
+  end
+  
+  def self.artist_sort_order 
+    self.first.artist_sort_order
+  end
+end
